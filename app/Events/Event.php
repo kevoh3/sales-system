f5e7168baa743050e55c8308f25d3a6c9ff0250e
Sergio Pomárico <sergiodavid21@gmail.com>
2016-07-24 14:31:57 -0500

    Basic configurations of laravel

---
+++ app/Events/Event.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace salesSystem\Events;
+
+abstract class Event
+{
+    //
+}
---
+++ app/Http/Requests/Request.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace salesSystem\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+abstract class Request extends FormRequest
+{
+    //
+}
